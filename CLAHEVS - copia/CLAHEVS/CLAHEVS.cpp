5569b06d998587cddeae52aaadb3cae800395f50
lgmore <lg_more@yahoo.com.ar>
2014-04-18 11:21:48 -0400

    version funcional, el json ahora incluye el peak signal to noise ratio de la imagen.

--- CLAHEVS - copia/CLAHEVS/CLAHEVS.cpp
+++ CLAHEVS - copia/CLAHEVS/CLAHEVS.cpp
@@ -43,6 +43,27 @@ using namespace cv;
 using namespace std;
 using cv::CLAHE;
 
+double getPSNR(const Mat& I1, const Mat& I2)
+{
+	Mat s1;
+	absdiff(I1, I2, s1);       // |I1 - I2|
+	s1.convertTo(s1, CV_32F);  // cannot make a square on 8 bits
+	s1 = s1.mul(s1);           // |I1 - I2|^2
+
+	Scalar s = sum(s1);         // sum elements per channel
+
+	double sse = s.val[0] + s.val[1] + s.val[2]; // sum channels
+
+	if (sse <= 1e-10) // for small values return zero
+		return 0;
+	else
+	{
+		double  mse = sse / (double)(I1.channels() * I1.total());
+		double psnr = 10.0*log10((255 * 255) / mse);
+		return psnr;
+	}
+}
+
 void logclahe(std::string msg)
 {
 	time_t now = time(0);
@@ -155,6 +176,7 @@ json::value getCLAHE(json::value parametros){
 	json::value retorno;
 	retorno[L"filas"] = json::value::number(dst.rows);
 	retorno[L"columnas"] = json::value::number(dst.cols);
+	retorno[L"psnr"] = json::value::number(getPSNR(m, dst));
 	int contador = 0;
 	json::value valores;
 	for (int j = 0; j<dst.rows; j++)
